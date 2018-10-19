bb1ab3c67e412536a61ca3a4fb05d942d9faf458
wangweiwei <xd8012@163.com>
2018-10-19 14:58:02 +0800

    Revert "test"
    This reverts commit 051fa694fabce0f012eb0124b33c7dfb9237ce48.

--- src/api/config.js
+++
@@ -1,4 +0,0 @@
-export const interfaceUrl = 'http://10.119.169.22:8290'
-export const businessPrefix = 'bs'
-export const systemPrefix = 'system'
-export const filePrefix = 'file'
--- src/pages/login/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-import {businessPrefix} from '../../api/config'
-
-export const loginByName = async (params) => {
-  const data = await axios.post(`/${businessPrefix}/authentication/authenticationByUserNameAndPassword`, params)
-  return data
-  // callback()
-}
